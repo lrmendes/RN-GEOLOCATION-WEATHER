affb7bcb995d75e57ab55b680b9964ff96e0e9d8
Lucas Mendes <lmendes@alunos.utfpr.edu.br>
2020-04-24 01:55:02 +0100

    Gelocation + Weather API - Ready

--- App.tsx
+++ App.tsx
@@ -1,26 +1,24 @@
 import React, {useState} from 'react';
-import { StyleSheet, Button, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View, ScrollView } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import * as Location from 'expo-location';
-
 
 export default function App() {
   const [location, setLocation] = useState(false);
   const [coordinates, setCoordinates] = useState({latitude: 0, longitude: 0});
-  const [weatherInfo, setWeatherInfo] = useState(null);
+  const [weatherInfo, setWeatherInfo] = useState({data: null, city: null});
 
-  // PUT YOUR API KEY HERE
-  const apiKey = "YOU API HERE";
+  // PUT YOUR API KEY HERE - GOT FROM: https://home.openweathermap.org/api_keys
+  const apiKey = "PUT YOUR API KEY HERE";
 
   async function getWeatherfromAPI() {
     try {
       let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${coordinates.latitude}&lon=${coordinates.longitude}&appid=${apiKey}`);
       let json = await response.json();
-      console.log(json);
-      setWeatherInfo(json);
+      console.log(json.city);
+      setWeatherInfo({data: json.list, city: json.city });
     } catch (error) {
-      console.error(error);
-      setWeatherInfo(null);
+      setWeatherInfo({data: null, city: null});
     }
   }
 
@@ -32,7 +30,11 @@ export default function App() {
     } else {
       setLocation(true);
       
-      let currentLocation = await Location.getCurrentPositionAsync({});
+      let currentLocation = await Location.getCurrentPositionAsync({
+        enableHighAccuracy: false,
+        timeout: 5000,
+        maximumAge: 10000
+      });
       setCoordinates({latitude: currentLocation.coords.latitude, longitude: currentLocation.coords.longitude});
     }
   }
@@ -63,8 +65,27 @@ export default function App() {
       }
 
       {
-        weatherInfo !== null
-        ? <Text>Weather Data Here</Text>
+        weatherInfo.data !== null
+        ? <View style={styles.weatherView}>
+            <Text style={styles.item}>City: {weatherInfo.city.name}</Text>
+            <Text style={styles.item}>Country: {weatherInfo.city.country}</Text>
+
+            <ScrollView style={{ width: '80%'}}>
+            {
+              weatherInfo.data.map( (item) => {
+              console.log("-",item);
+              return (
+                <View style={styles.card} key={item.dt}>
+                <Text style={styles.item2}>Date: {item.dt_txt }</Text>
+                <Text style={styles.item2}>Temperature: { (item.main.temp -273.15).toFixed(2) } °C</Text>
+                <Text style={styles.item2}>Description: {item.weather[0].description }</Text>
+                </View>
+              )
+              })
+            }
+            </ScrollView>
+
+          </View>
         : null
       }
 
@@ -73,13 +94,32 @@ export default function App() {
 }
 
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-    padding:22,
+    paddingTop:80,
   },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  item2: {
+    padding: 5,
+    fontSize: 18,
+    height: 44,
+  },
+  weatherView: {
+    width: '100%',
+    height: 400, 
+    alignItems:'center',
+    paddingTop: 25,
+  },
+  card: {
+    marginTop: 10,
+    width: '100%',
+    backgroundColor: '#ede8e8'
+  }
 });
--- README.md
+++ README.md
@@ -1,7 +1,27 @@
-# RN-GEOLOCATION-WEATHER
-That is a Basic project in React Native to capture user's location and get the current weather of that location from openweathermap API.
+# React Native - Geolocation + Weather
+This is a Basic project in React Native to capture user's location and get the current weather of that location from openweathermap API.
+
+#### System running example:
+
+![](Example.gif)
+
+### Requisits:
+The project uses Expo and two modules to get smarthphone location:
 
 expo install expo-permissions
 expo install expo-location
 
-expo start
+The project uses OpenWeatherMap API to get the current weather based on the user's latitude and longitude.
+
+You needs an API-KEY from OpenWeatherMap to use the Weather API, you can create a free account and put your API-KEY in the "APP.tsx" file.
+The API call used in the project it's free and gives the weather about every 3 hours of the next 5 days, totalizing 40 blocks of weather info.
+https://openweathermap.org/api
+https://openweathermap.org/forecast5
+
+### Install:
+
+You can install by cloning this repository and running "yarn install" inside that directory.
+
+### Running:
+
+You can run this app by running "expo start" on a terminal, in the project directory.
